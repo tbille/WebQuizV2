998f05e77655f1a381cbf04c0822eeb3fcb609cd
Garing <garingtang@gmail.com>
2014-11-21 13:25:27 -0500

    added connect info in db

--- TP5/lib/db.js
+++ TP5/lib/db.js
@@ -1,9 +1,18 @@
-var mongoose = require( 'mongoose' );
-var Schema   = mongoose.Schema;
- 
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
 module.exports.mongoose = mongoose;
 module.exports.Schema = Schema;
 
 // Connect to cloud database
-mongoose.connect("mongodb://webquizdb:webquizdb@ds053140.mongolab.com:53140/webquizdb");
+var username = "webquizdb"
+var password = "webquizdb";
+var address = '@ds053140.mongolab.com:53140/webquizdb';
+connect();
 
+// Connect to mongo
+function connect() {
+  var url = 'mongodb://' + username + ':' + password + address;
+  mongoose.connect(url);
+}
+function disconnect() {mongoose.disconnect()}
