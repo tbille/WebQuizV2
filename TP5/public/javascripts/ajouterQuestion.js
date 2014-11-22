cf2065da61654ca829137724d0b06d0887e301dc
Garing <garingtang@gmail.com>
2014-11-22 17:27:56 -0500

    modified add question placeholder input with increment

--- TP5/public/javascripts/ajouterQuestion.js
+++ TP5/public/javascripts/ajouterQuestion.js
@@ -1,8 +1,12 @@
 //Ajouter un input field lorsqu'on clique sur Ajouter une reponse
 $(document).ready(function() {
         var scntDiv = $('.allReponses');   
+        var n = 3;
+  
         $('.ajouterReponse').click(function() {
-               $(scntDiv).append('<div><input type="text" placeholder="Réponse #{n++}"/><input type="radio" name="answer" /></div>');
+        //var n=99;         
+        $(scntDiv).append("<div><input type=text placeholder='Réponse "+ ++n +" '/><input type=radio name=answer/></div>");      
+          
         });
         
 });
