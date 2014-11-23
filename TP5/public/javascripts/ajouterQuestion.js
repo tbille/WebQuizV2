3add066ee8bc47e0ed2c5d64207a4af61696ab76
Garing <garingtang@gmail.com>
2014-11-23 17:11:40 -0500

    added remove input link and validation on /ajouterQuestion

--- TP5/public/javascripts/ajouterQuestion.js
+++ TP5/public/javascripts/ajouterQuestion.js
@@ -1,19 +1,58 @@
-//Ajouter un input field lorsqu'on clique sur Ajouter une reponse
+
 $(document).ready(function() {
-        var scntDiv = $('.allReponses');   
-        var n = 3;
+              
+  var scntDiv = $('.allReponses');   
+  var n = 3;
  
-         $('.ajouterReponse').click(function() {
-        $(scntDiv).append("<div><input type=text size=63 name=answers placeholder='Réponse no. "+ ++n +" '/></div>"); 
-        $('#chooseAnswer').attr('max', n);
-        });
+  //Ajouter un input field lorsqu'on clique sur Ajouter une reponse
+    $('.ajouterReponse').click(function() {
+        $(scntDiv).append("<div><input class=listeReponses type=text size=60 name=answers required placeholder='Réponse no. "+ ++n +" '/><a href=# class=remove_field> X</a></div>"); 
+        var nbInputs = $('.listeReponses').length;
+        $('#chooseAnswer').attr('max', nbInputs); //modifie le max de l'index bonne reponse en fonction du nombre de input reponses
+        $('#chooseAnswer').attr('min', 1); 
+    });
   
+  //ajouter un lien supprimer un input field
+    $(scntDiv).on("click",".remove_field", function(e){ 
+        e.preventDefault(); $(this).parent('div').remove(); n--;
+        var nbInputs = $('.listeReponses').length;
+        $('#chooseAnswer').attr('max', nbInputs); //modifie le max de l'index bonne reponse en fonction du nombre de input reponses
+        $('#chooseAnswer').attr('min', 1);
+    })
+
         /* Retiré input avec bouton radio
         $('.ajouterReponse').click(function() {
         $(scntDiv).append("<div><input type=text placeholder='Réponse "+ ++n +" '/><input type=radio name=answer/></div>");      
         });
-        */
+        */ 
+
+  //Valider si le domaine inscrit est HTML CSS ou JavaScript
+    $("input[type='submit']").click(function() {
+        var selectedDomain = $('#selectedDomain').val();
+        alert(selectedDomain);
+      
+      /*  if(selectedDomain == "HTML" || selectedDomain == "CSS") {
+         
+        }*/
+       
+    });
+        
+        
+
 });
+
+
+
+
+
+
+
+
+
+
+
+  
+
 
 
 
